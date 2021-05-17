462ec708d2d0a1b59caf62954a662ac6733d4847
Коваленко Илья Григорьевич <igkovale@mts.ru>
2021-05-17 15:57:46 +0300

    function type

--- metanit/func/main.go
+++ metanit/func/main.go
@@ -4,6 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println(add(1, 2, "Иван", "Иванов"))
+
+	var funcPtr func(int, int) int = simpleAdd
+
+	fmt.Println("Additions")
+	printFunc(funcPtr)
+	fmt.Println("Multiplications")
+	printFunc(multiply)
+
 }
 
 func add(x, y int, firstName, lastName string) (sum int, fullName string) {
@@ -11,3 +19,12 @@ func add(x, y int, firstName, lastName string) (sum int, fullName string) {
 	fullName = firstName + " " + lastName
 	return
 }
+
+func printFunc(funcPtr func(int, int) int) {
+	var numbers = [...]int{1, 2, 3}
+	for _, number := range numbers {
+		fmt.Println(funcPtr(number, number))
+	}
+}
+func simpleAdd(x, y int) int { return x + y }
+func multiply(x, y int) int  { return x * y }
