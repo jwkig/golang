780e84acbafedc9ef8db5ed1f8d2741748066fe9
Коваленко Илья Григорьевич <igkovale@mts.ru>
2021-05-17 12:22:02 +0300

    loops with continue and break

--- metanit/loops/main.go
+++ metanit/loops/main.go
@@ -10,7 +10,7 @@ func main() {
 		fmt.Println()
 	}
 
-	var numbers = [...]int{1, 2, 3, 4, 5}
+	var numbers = [10]int{1, -2, 3, -4, 5, -6, -7, 8, -9, 10}
 
 	for _, num := range numbers {
 		fmt.Println(num)
@@ -20,4 +20,25 @@ func main() {
 	for i := 0; i < len(users); i++ {
 		fmt.Println(users[i])
 	}
+
+	var sum = 0
+
+	for _, value := range numbers {
+		if value < 0 {
+			continue // переходим к следующей итерации
+		}
+		sum += value
+	}
+	fmt.Println("Sum:", sum)
+
+	numbers = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	sum = 0
+
+	for _, value := range numbers {
+		if value > 4 {
+			break // если число больше 4 выходим из цикла
+		}
+		sum += value
+	}
+	fmt.Println("Sum:", sum) // Sum: 10
 }
