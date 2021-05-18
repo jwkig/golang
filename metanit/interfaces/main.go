0f9cbe6518e4ffec0ac8e1b4736571d6374f4d07
Коваленко Илья Григорьевич <kig@narod.ru>
2021-05-18 17:07:11 +0300

    interfaces

--- metanit/interfaces/main.go
+++ metanit/interfaces/main.go
@@ -12,6 +12,43 @@ type Car struct {
 type Aircraft struct {
 }
 
+type Stream interface {
+	read() string
+	write(string)
+	close()
+}
+
+func writeToStream(stream Stream, text string) {
+	stream.write(text)
+}
+
+func closeStream(stream Stream) {
+	stream.close()
+}
+
+type File struct {
+	text string
+}
+
+type Folder struct{}
+
+func (f *File) read() string {
+	return f.text
+}
+
+func (f *File) write(s string) {
+	f.text = s
+	fmt.Println("Запись в файл строки", s)
+}
+
+func (f *File) close() {
+	fmt.Println("Файл закрыт")
+}
+
+func (f *Folder) close() {
+	fmt.Println("Папка закрыта")
+}
+
 func (car Car) move() {
 	fmt.Println("Car moving")
 }
@@ -26,4 +63,12 @@ func main() {
 
 	car.move()
 	plane.move()
+
+	file := &File{}
+	folder := &Folder{}
+
+	writeToStream(file, "Hello world")
+	closeStream(file)
+
+	folder.close()
 }
