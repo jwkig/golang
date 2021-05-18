a969de171e949bb86e0c7083cef4d90802bf1022
Коваленко Илья Григорьевич <kig@narod.ru>
2021-05-18 13:58:49 +0300

    linked list

--- metanit/struct/main.go
+++ metanit/struct/main.go
@@ -8,22 +8,36 @@ type contact struct {
 }
 
 type person struct {
-	name        string
-	age         int
-	contactInfo contact
+	name string
+	age  int
+	contact
+}
+
+type node struct {
+	value int
+	next  *node
+}
+
+func printNodeValue(n *node) {
+
+	fmt.Println(n.value)
+	if n.next != nil {
+		printNodeValue(n.next)
+	}
 }
 
 func main() {
 	var tom = person{
 		name: "Tom",
 		age:  24,
-		contactInfo: contact{
+		contact: contact{
 			email: "tom@gmail.com",
 			phone: "+1234567899",
 		},
 	}
 	fmt.Println(tom.name)
 	fmt.Println(tom.age)
+	fmt.Println(tom.email)
 
 	tom.age = 38
 	fmt.Println(tom.name, tom.age)
@@ -33,4 +47,18 @@ func main() {
 	fmt.Println(tom.age)
 	(*tomPointer).age = 32
 	fmt.Println(tom.age)
+
+	first := node{value: 4}
+	second := node{value: 5}
+	third := node{value: 6}
+
+	first.next = &second
+	second.next = &third
+
+	var current *node = &first
+	for current != nil {
+		fmt.Println(current.value)
+		current = current.next
+	}
+
 }
