5e8d36ada023e6a0a69de8cfd5dae8ebd5dea08e
Коваленко Илья Григорьевич <kig@narod.ru>
2021-05-18 17:07:47 +0300

    nested interfaces

--- metanit/interfaces/main.go
+++ metanit/interfaces/main.go
@@ -2,73 +2,43 @@ package main
 
 import "fmt"
 
-type Vehicle interface {
-	move()
+type Reader interface {
+	read()
 }
 
-type Car struct {
-}
-
-type Aircraft struct {
-}
-
-type Stream interface {
-	read() string
+type Writer interface {
 	write(string)
-	close()
 }
 
-func writeToStream(stream Stream, text string) {
-	stream.write(text)
+type ReaderWriter interface {
+	Reader
+	Writer
 }
 
-func closeStream(stream Stream) {
-	stream.close()
+func writeToStream(writer Writer, text string) {
+	writer.write(text)
+}
+func readFromStream(reader Reader) {
+	reader.read()
 }
 
 type File struct {
 	text string
 }
 
-type Folder struct{}
-
-func (f *File) read() string {
-	return f.text
+func (f *File) read() {
+	fmt.Println(f.text)
 }
-
-func (f *File) write(s string) {
-	f.text = s
-	fmt.Println("Запись в файл строки", s)
-}
-
-func (f *File) close() {
-	fmt.Println("Файл закрыт")
-}
-
-func (f *Folder) close() {
-	fmt.Println("Папка закрыта")
-}
-
-func (car Car) move() {
-	fmt.Println("Car moving")
-}
-
-func (plain Aircraft) move() {
-	fmt.Println("Aircraft flying")
+func (f *File) write(message string) {
+	f.text = message
+	fmt.Println("Запись в файл строки", message)
 }
 
 func main() {
-	var car Car = Car{}
-	var plane Aircraft = Aircraft{}
 
-	car.move()
-	plane.move()
-
-	file := &File{}
-	folder := &Folder{}
-
-	writeToStream(file, "Hello world")
-	closeStream(file)
-
-	folder.close()
+	myFile := &File{}
+	writeToStream(myFile, "hello world")
+	readFromStream(myFile)
+	writeToStream(myFile, "lolly bomb")
+	readFromStream(myFile)
 }
